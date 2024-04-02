34b57360c71a3bb714d8768ba40525fc02502d62
william <122491670+wjgarciam@users.noreply.github.com>
2024-04-02 17:45:54 -0300

    Change some class names to better reflect what they represent

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
 const padContainer = document.querySelector("#container");
 let size = 25
 createGrid(size)
-
 function createGrid(size) {
         for (let i = 0; i < (size*size); i++) {
-        let columns = document.createElement("div")
-        columns.classList.add("columns")
-        columns.style.backgroundColor = "red"
-        columns.style.width = (650/size) + "px"
-        padContainer.appendChild(columns);
+        let gridSquare = document.createElement("div")
+        gridSquare.classList.add("gridSquare")
+        gridSquare.style.backgroundColor = "red"
+        gridSquare.style.width = (650/size) + "px"
+        padContainer.appendChild(gridSquare);
     }
 }
