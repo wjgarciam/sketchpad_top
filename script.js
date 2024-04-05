875fe3cae620c6d7cdec74cc049c229eab608044
william <122491670+wjgarciam@users.noreply.github.com>
2024-04-05 08:27:16 -0300

    Add the code so that the grid divs change color when the mouse passes over them

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const padContainer = document.querySelector("#container");
-let size = 25
+let size = 16
 createGrid(size)
 function createGrid(size) {
         for (let i = 0; i < (size*size); i++) {
         let gridSquare = document.createElement("div")
         gridSquare.classList.add("gridSquare")
-        gridSquare.style.backgroundColor = "red"
         gridSquare.style.width = (650/size) + "px"
         padContainer.appendChild(gridSquare);
     }
+}
+const gridSq = padContainer.querySelectorAll(".gridSquare")
+for (const square of gridSq) {
+    square.addEventListener("mouseover", mOver);}
+    
+function mOver () {
+    event.target.classList.add("over")
 }
